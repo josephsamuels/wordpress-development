af3de9cf5eed1fd5fb79ac26864f99931280e8b9
Joseph Samuels <joseph.w.samuels@gmail.com>
2021-04-12 08:04:40 -0400

    main - Added phpmyadmin to docker-compose. Cleaned up wordpress-command.sh

--- scripts/wordpress-command.sh
+++ scripts/wordpress-command.sh
@@ -8,29 +8,27 @@ while [ ! -f "/usr/local/bin/wp" -a ! -f "wp" ]; do sleep 1; done; sleep 1;
 # Verify and move wp to bin.
 test -f "wp" && mv /var/www/html/wp /usr/local/bin/
 
-# mkdir /var/www/html/wp-content/upgrade
-
+# If Wordpress isn't installed yet...
 if [ ! -f wp-config.php ]; then
   wp --allow-root core download
-fi
 
-# If wordpress is not installed...
-if ! $(wp --allow-root core is-installed); then
-  cp wp-config-sample.php wp-config.php
+  if ! $(wp --allow-root core is-installed); then
+    cp wp-config-sample.php wp-config.php
 
-  wp --allow-root config set DB_HOST $WORDPRESS_DB_HOST
-  wp --allow-root config set DB_NAME $WORDPRESS_DB_NAME
-  wp --allow-root config set DB_USER $WORDPRESS_DB_USER
-  wp --allow-root config set DB_PASSWORD $WORDPRESS_DB_PASSWORD
-  wp --allow-root config set WP_DEBUG true --raw
-  wp --allow-root config set SCRIPT_DEBUG true --raw
-  wp --allow-root config set FS_METHOD direct
+    wp --allow-root config set DB_HOST $WORDPRESS_DB_HOST
+    wp --allow-root config set DB_NAME $WORDPRESS_DB_NAME
+    wp --allow-root config set DB_USER $WORDPRESS_DB_USER
+    wp --allow-root config set DB_PASSWORD $WORDPRESS_DB_PASSWORD
+    wp --allow-root config set WP_DEBUG true --raw
+    wp --allow-root config set SCRIPT_DEBUG true --raw
+    wp --allow-root config set FS_METHOD direct
 
-  wp --allow-root core install --path="/var/www/html" --url="http://localhost:8080" --title="wp-dev" --admin_user=wordpress --admin_password=wordpress --admin_email=admin@test.com
+    wp --allow-root core install --path="/var/www/html" --url="http://localhost:8080" --title="wp-dev" --admin_user=wordpress --admin_password=wordpress --admin_email=admin@test.com
 
-  wp --allow-root rewrite structure '/%postname%/' --hard
+    wp --allow-root rewrite structure '/%postname%/' --hard
 
-  mkdir /var/www/html/wp-content/upgrade
+    mkdir /var/www/html/wp-content/upgrade
+  fi
 fi
 
 # Clear out default plugins.
